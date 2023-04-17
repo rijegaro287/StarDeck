2c0fc82d9cd475ee00bdbd25cd4e7d32a3cfcde4
isriom <Isriom@estudiantec.cr>
2023-04-16 20:57:46 -0600

    Add models

---
+++ Models/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stardeck.Models;
+
+public partial class Account
+{
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Nickname { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Country { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public bool? Active { get; set; } = true;
+
+    public long Avatar { get; set; }
+
+    public string? Config { get; set; }
+
+    public long Points { get; set; }
+
+    public long Coins { get; set; }
+
+    public virtual Avatar AvatarNavigation { get; set; } = null!;
+
+    public virtual Deck? Deck { get; set; }
+
+    public virtual ICollection<Avatar> Avatars { get; set; } = new List<Avatar>();
+}
---
+++ Models/Deck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stardeck.Models;
+
+public partial class Deck
+{
+    public string IdAccount { get; set; } = null!;
+
+    public string Deck1 { get; set; } = null!;
+
+    public virtual Account IdAccountNavigation { get; set; } = null!;
+}
