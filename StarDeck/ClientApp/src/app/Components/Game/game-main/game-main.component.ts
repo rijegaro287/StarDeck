e9dbbf75657e9c6c7a812b2903899c77afc7b759
Ricardo Gatgens <gatgens48@gmail.com>
2023-05-15 16:39:49 -0600

    Botones de rendición y terminar turno

--- StarDeck/ClientApp/src/app/Components/Game/game-main/game-main.component.ts
+++ StarDeck/ClientApp/src/app/Components/Game/game-main/game-main.component.ts
@@ -5,6 +5,8 @@ import * as Phaser from 'phaser';
 import Card from '../GameObjects/Card';
 import CardList from '../GameObjects/CardList';
 import HiddenCard from '../GameObjects/HiddenCard';
+import Button from '../GameObjects/Button';
+import { COLORS } from '../Constants';
 
 @Component({
   selector: 'app-game-main',
@@ -51,6 +53,7 @@ class MainScene extends Phaser.Scene {
     this.playableWidth = Number(this.sys.game.canvas.width) - 2 * this.margin;
     this.playableHeight = Number(this.sys.game.canvas.height) - 2 * this.margin;
 
+    this.load.image('main-bg', '../../../assets/images/main-bg.jpg');
     this.load.image('default-card', '../../../assets/images/card.png');
     this.load.image('hidden-card', '../../../assets/images/logo.png');
     this.load.image('energy-icon', '../../../assets/svg/energy-icon.svg');
@@ -58,6 +61,11 @@ class MainScene extends Phaser.Scene {
   }
 
   create() {
+    const backgroundImage = this.add.image(0, 0, 'main-bg');
+    backgroundImage.setDisplaySize(Number(this.sys.game.canvas.width), Number(this.sys.game.canvas.height));
+    backgroundImage.setOrigin(0, 0);
+
+
     const card1 = new Card(this, 0, 0, 'Carta 1', 'Raza 1', 50, 75);
     const card2 = new Card(this, 0, 0, 'Carta 1', 'Raza 1', 50, 75);
     const card3 = new Card(this, 0, 0, 'Carta 1', 'Raza 1', 50, 75);
@@ -78,13 +86,30 @@ class MainScene extends Phaser.Scene {
     const playerDeck = new HiddenCard(this, 0, 0);
     const playerDeckHeight = playerDeck.height + 2 * this.margin;
     const playerDeckY = playerHandY
-    const playerDeckX = playerHandWidth + 2 * this.margin + this.playableWidth * 0.08 / 2;
+    const playerDeckX = playerHandWidth + 1.5 * this.margin + this.playableWidth * 0.08 / 2;
 
     playerDeck.scale = playerHandHeight / playerDeckHeight
     playerDeck.setPosition(playerDeckX, playerDeckY)
 
+    const endTurnButton = new Button(
+      this, 0, 0, this.playableWidth / 10, 35, 'Terminar turno', COLORS.PRIMARY
+    );
+    const endTurnButtonPositionX = this.playableWidth - endTurnButton.width / 2;
+    const endTurnButtonPositionY = this.playableHeight + 2 * this.margin - playerHandHeight - endTurnButton.height;
+    endTurnButton.setPosition(endTurnButtonPositionX, endTurnButtonPositionY)
+
+    const surrenderButton = new Button(
+      this, 0, 0, this.playableWidth / 10, 35, 'Rendición', COLORS.WARNING
+    );
+    const surrenderButtonPositionX = surrenderButton.width / 2 + this.margin;
+    const surrenderButtonPositionY = this.playableHeight + 2 * this.margin - playerHandHeight - surrenderButton.height;
+    surrenderButton.setPosition(surrenderButtonPositionX, surrenderButtonPositionY)
+
+
     this.add.existing(playerHandContainer);
     this.add.existing(playerDeck);
+    this.add.existing(endTurnButton);
+    this.add.existing(surrenderButton);
   }
 
   update() {
